4b9de9506e2553256ee7188b470018444daf0768
venkat gadicherla <venkat_gadicherla@hotmail.com>
2019-03-22 13:56:25 +1100

    Add project files.

---
+++ OnboardingTask2/Models/ProductSoldViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnboardingTask2.Models
+{
+    public class ProductSoldViewModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public int StoreId { get; set; }
+        public string Date { get; set; }
+    }
+}
